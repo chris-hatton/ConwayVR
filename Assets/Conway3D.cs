1fd9c07a15190c0e691d39b7c042cb8c9440acd5
Chris Hatton <christopherhattonuk@gmail.com>
2017-02-11 13:35:40 +1100

    Initial commit

---
+++ Assets/Conway3D.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public delegate void PositionAction(VectInt3 offset);
+
+public class Conway3D : MonoBehaviour
+{
+	public int dieFromExposureThreshold = 3;
+	public int dieFromOvercrowdingThreshold = 3;
+	public int propagateNumber = 8;
+
+    static readonly int size = 16;
+
+    private bool[,,,] cells = new bool[2, size, size, size];
+
+    int population = 0;
+	int lastPopulation = 0;
+	int generation = 0;
+
+	int steadyGenerationLimit = 5;
+	int steadyGenerationCount = 0;
+
+    bool tickTock = false;
+    float generationInterval = 0.2f;
+
+    public void SetNextCell(VectInt3 at, bool willBeAlive)
+    {
+        cells[tickTock ? 1 : 0, at.x, at.y, at.z] = willBeAlive;
+    }
+
+    public bool GetCurrentCell(VectInt3 at)
+    {
+        return cells[tickTock ? 0 : 1, at.x, at.y, at.z];
+    }
+
+	public void SetCurrentCell(VectInt3 at, bool isAlive)
+	{
+		cells[tickTock ? 0 : 1, at.x, at.y, at.z] = isAlive;
+	}
+
+    private int CreateSeed()
+    {
+        const int seedSize = 4;
+        int seedPopulation = 0;
+
+        PositionAction maybeSet = position =>
+        {
+            bool isPresent = Random.value >= 0.8;
+            if (isPresent) ++seedPopulation;
+            SetCurrentCell(position, isPresent);
+        };
+
+		VectInt3.Iterate3D(offset: (int)((size-seedSize)/2), length: seedSize, action: maybeSet);
+
+        return seedPopulation;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        population = CreateSeed();
+
+        StartCoroutine(Evolve());
+    }
+
+    IEnumerator Evolve()
+    {
+        while (population > 0) // Keep evolving while there are any cells left...
+        {
+			float evolutionStartTime = Time.time;
+			float evolutionEndTime =
+				evolutionStartTime + generationInterval; // This will determine how long we should wait at the end
+		
+			UpdateScene();
+
+			population = 0;
+
+            PositionAction evolvePosition = position =>
+            {
+                int neighbourCount = 0;
+
+                PositionAction countNeighbours = countOffset =>
+                {
+                    var countPosition = (position + countOffset).WrapTo(offset: 0, length: size);
+                    var isPresent = GetCurrentCell(countPosition);
+                    if (isPresent)
+                    {
+                        ++neighbourCount;
+                    }
+                };
+
+                VectInt3.Iterate3D(-1, 3, countNeighbours);
+
+                bool isAlive = GetCurrentCell(position);
+                bool willBeAlive;
+
+                if (isAlive)
+                {
+					if (neighbourCount < dieFromExposureThreshold) // Die from exposure?
+                    {
+                        willBeAlive = false;
+                    }
+					else if (neighbourCount > dieFromOvercrowdingThreshold) // Die from overcrowding?
+                    {
+                        willBeAlive = false;
+                    }
+                    else // Live on!
+                    {
+                        willBeAlive = true;
+                    }
+                }
+                else // No cell here yet: will any neighbours propagate?
+                {
+					willBeAlive = neighbourCount == propagateNumber;
+                }
+
+				if( willBeAlive )
+				{
+					
+					++population;
+				}
+
+                SetNextCell(at: position, willBeAlive: willBeAlive);
+            };
+
+            VectInt3.Iterate3D(offset: 0, length: size, action: evolvePosition);
+
+            tickTock = !tickTock;
+
+			if (lastPopulation == population)
+			{
+				++steadyGenerationCount;
+				if (steadyGenerationCount >= steadyGenerationLimit)
+				{
+					#if UNITY_EDITOR
+					UnityEditor.EditorApplication.isPlaying = false;
+					#else
+					Application.Quit();
+					#endif 
+				}
+			}
+			else
+			{
+				steadyGenerationCount = 0;
+			}
+
+			lastPopulation = population;
+
+			float waitTime = evolutionEndTime - Time.time;
+
+			Debug.Log (string.Format("Generation {0}, population: {1}, waitTime: {2}", generation, population, waitTime));
+
+			++generation;
+
+
+            yield return new WaitForSeconds(Math.Max(0, waitTime));
+        }
+    }
+
+    // Display
+
+    private static float displayScale = 0.5f;
+
+    private GameObject[,,] cellCubes = new GameObject[size, size, size];
+
+    public void SetCellCube(VectInt3 at, bool isAlive)
+    {
+        GameObject existingCube = cellCubes[at.x, at.y, at.z];
+
+        bool isPresent = (existingCube != null);
+        bool shouldBePresent = GetCurrentCell(at: at);
+
+        if (isPresent == shouldBePresent)
+        {
+            return;
+        }
+
+        if (shouldBePresent) // Birth a new cube
+        {
+            GameObject newCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            newCube.transform.position = new Vector3(at.x * displayScale, at.y * displayScale, at.z * displayScale);
+            newCube.transform.localScale = new Vector3(displayScale, displayScale, displayScale);
+            cellCubes[at.x, at.y, at.z] = newCube;
+        }
+        else // Remove the dead cube
+        {
+            Destroy(existingCube);
+        }
+    }
+
+    private void UpdateScene()
+    {
+        PositionAction refreshCube = position =>
+        {
+            SetCellCube(at: position, isAlive: GetCurrentCell(at: position));
+        };
+
+        VectInt3.Iterate3D(offset: 0, length: size, action: refreshCube);
+    }
+}
---
+++ Assets/VectInt3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public struct VectInt3
+{
+	public int x,y,z;
+
+	public VectInt3( int x, int y, int z )
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static void Iterate3D( int offset, int length, PositionAction action )
+	{
+		int end = length + offset;
+
+		for( int z = offset; z < end; ++z )
+		{
+			for( int y = offset; y < end; ++y )
+			{
+				for( int x = offset; x < end; ++x )
+				{
+					action( new VectInt3 (x, y, z) );
+				}
+			}
+		}
+	}
+
+	public VectInt3 WrapTo( int offset, int length )
+	{
+		return new VectInt3 (
+			mod((x - offset), length) + offset,
+			mod((y - offset), length) + offset,
+			mod((z - offset), length) + offset
+		);
+	}
+
+	private static int mod(int value, int modulo)
+	{
+		return (value%modulo + modulo)%modulo;
+	}
+
+	public static VectInt3 operator +( VectInt3 vectInt3a, VectInt3 vectInt3b) 
+	{
+		return new VectInt3( (vectInt3a.x + vectInt3b.x), (vectInt3a.y + vectInt3b.y), (vectInt3a.z + vectInt3b.z) );
+	} 
+}
